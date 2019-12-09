54adff0ed728859a55b00f474db06ac319c25ce4
Michelle Quinn <michelle.n.quinn@gmail.com>
2019-12-09 16:06:26 -0600

    getting axios to work

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ function generateHTML(data, color, starCount) {
       margin-right: 0;
       margin-left: 0;
     }
-    .card-body{
+    .section-content{
       background-color: ${color};
       color:white;
-      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
       padding: 10px;
       margin: 20px;
       text-align: center;
@@ -39,47 +38,23 @@ function generateHTML(data, color, starCount) {
       <div class="container">
       <!--main card body-->
           <div class="row">
-          <div class="col-lg-9 card-body">
+          <div class="col-lg-9 section-content">
                   <img src="${data.avatar_url}" alt="...">
                   <h2> Hi! </h2>
                   <h2> My name is ${data.name}</h2>
-                  <h5> Currently @ ${data.company}</h5>
-                  <h6> 
-                    <a href="https://www.google.com/maps/place/${data.location}" target="_blank"><svg id="i-location" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                      <circle cx="16" cy="11" r="4" />
-                      <path d="M24 15 C21 22 16 30 16 30 16 30 11 22 8 15 5 8 10 2 16 2 22 2 27 8 24 15 Z" />
-                  </svg> ${data.location}</a>
-                    <a href=${data.html_url} target="_blank">
-                      <svg id="i-github" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="32" height="32">
-                        <path stroke-width="0" fill="currentColor" d="M32 0 C14 0 0 14 0 32 0 53 19 62 22 62 24 62 24 61 24 60 L24 55 C17 57 14 53 13 50 13 50 13 49 11 47 10 46 6 44 10 44 13 44 15 48 15 48 18 52 22 51 24 50 24 48 26 46 26 46 18 45 12 42 12 31 12 27 13 24 15 22 15 22 13 18 15 13 15 13 20 13 24 17 27 15 37 15 40 17 44 13 49 13 49 13 51 20 49 22 49 22 51 24 52 27 52 31 52 42 45 45 38 46 39 47 40 49 40 52 L40 60 C40 61 40 62 42 62 45 62 64 53 64 32 64 14 50 0 32 0 Z" />
-                    </svg> Git</a>
-                    <a href=${data.blog} target="_blank">
-                      <svg id="i-feed" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                        <circle cx="6" cy="26" r="2" fill="currentColor" />
-                        <path d="M4 15 C11 15 17 21 17 28 M4 6 C17 6 26 15 26 28" />
-                    </svg> Blog</a>
-                  </h6>
               </div>
           </div>
-          <div class="row bio">
-            <h3>${data.bio}</h3>
-          </div>
           <div class="row text-center">
-              <div class="col-lg-4 card-body">
+              <div class="col-lg-4 section-content">
                 <h2>Public Respositories</h2>
                 <h4>${data.public_repos}</h4>
               </div>
-              <div class="col-lg-4 card-body">
+              <div class="col-lg-4 section-content">
                   <h2>Followers</h2>
                   <h4>${data.followers}</h4>
                 </div> 
           </div>
-          <div class="row">
-              <div class="col-lg-4 card-body">
-                <h2>Github Stars</h2>
-                <h4>${starCount}</h4>
-              </div>
-              <div class="col-lg-4 card-body">
+              <div class="col-lg-4 section-content">
                   <h2>Following</h2>
                   <h4>${data.following}</h4>
                 </div> 
@@ -89,33 +64,32 @@ function generateHTML(data, color, starCount) {
   </html>`
 }
 
-function promptUser() {
+function userInput() {
   inquirer
     .prompt([
       {
         message: "Enter your GitHub username:",
-        name: "username"
+        name: "name"
       },
       {
-        message: "Enter your favourite color:",
-        name: "color"
+        type: "list",
+        message: "Enter your favorite color:",
+        name: "color",
+        choices: ["Blue", "Green", "Pink", "Purple", "Yellow", "Red"]
       }
     ])
-    .then(function ({ username, color }) {
-      const queryUrl = `https://api.github.com/users/${username}`;
+    .then(function ({ name, color }) {
+      const queryUrl = `https://api.github.com/users/${name}`;
       console.log(queryUrl)
 
       axios
         .get(queryUrl)
         .then(function (res) {
-            const queryUrl = `https://api.github.com/users/${username}/repos`;
+            const queryUrl = `https://api.github.com/users/${name}/repos`;
             axios
             .get(queryUrl)
-            .then(function (response) {
-               let result = response.data.map(repo => repo.stargazers_count);
-               let count = result.reduce((a, b) => a + b, 0);
-              //  console.log(count)
-               const profile = generateHTML(res.data, color, count);
+            .then(function () {
+               const profile = generateHTML(res.data, color);
               console.log()
                fs.writeFile("./profile.html", profile, function (err) {
                  if (err) {
@@ -140,4 +114,4 @@ function promptUser() {
 
 }
 
-promptUser();
+userInput();
